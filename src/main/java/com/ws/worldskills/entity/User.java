28fc1eeb72d5bb16379e2717836c23dce7a4c6a0
User <User>
2021-10-16 12:01:23 +0300

    add db

--- src/main/java/com/ws/worldskills/entity/User.java
+++ src/main/java/com/ws/worldskills/entity/User.java
@@ -2,13 +2,11 @@ package com.ws.worldskills.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "usr")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
